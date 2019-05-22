a9f3b479bd9e8eca78775d7eecabd445364067bd
primekjt <primekjt@douzone.com>
2019-05-22 09:50:57 +0900

    로컬PC 업데이트

---
+++ PythonFile/Work/더존NSM발주정보확인_ver2.py
@@ -0,0 +1,156 @@
+﻿import pandas as pd
+#import xlrd
+import os
+import re
+import datetime
+
+def get_filter_dataframe(df):
+    read_columns = ['주담당채널', '영업채널명', '영업담당자명', '고객명', '사업자/주민', '주문일자', '주문상태', '주문현황', '납입구분', '상품명', '단위상품명', '공급가',
+                    '금액(VAT별도)', '판매유형', '기회시작일']
+
+    df1 = df[read_columns]
+
+    # 집합(중복값 불허)
+    product_category = {'ICUBE 클라우드서버', 'ICUBE/IU 클라우드서버', 'ICUBE/GW 클라우드서버', 'IU 클라우드서버'
+        , 'IU/GW 클라우드서버', 'GW 클라우드서버', 'Private Cloud', 'iU Private', 'iCUBE Private'
+        , '클라우드서버', '클라우드백업서버', 'Bizbox Alpha Cloud', '(신)iCUBE Cloud Edition'}
+
+    df2 = df1[df1['상품명'].isin(product_category)]
+
+    groupby_columns_list = ['영업채널명', '영업담당자명', '고객명', '사업자/주민', '주문일자', '상품명', '판매유형']
+    grouped = df2.groupby(groupby_columns_list)
+    df3 = grouped.sum().reset_index()
+
+    # IT코디센터는 C로 대리점은 B로 새로운칼럼에 추가한다
+    channel_series = df3['영업채널명'].apply(
+        lambda v: 'C' if ((v.endswith('IT코디센터')) | (v == '강원지점') | (v == '제주지점')) else 'B')
+    # df3['CB'] = channel_series
+    df3.loc[:, 'C_B'] = channel_series
+
+    # sort
+    df4 = df3.sort_values(by=['C_B', '주문일자', '영업채널명', '영업담당자명', '고객명'], ascending=[False, True, True, True, True])
+
+    # 업체명/솔루션/년금액/센터/담당자/판매유형
+    """
+    regex = re.compile(r'.*[^\s?IT코디센터$]') # space와 IT코디센터가 아닌것을 선택
+    price_sum = 0
+    for row in df3.values:
+       text = row[0]
+       match_obj = regex.search(text)
+       if match_obj:
+          it_coodi = match_obj.group().replace('/경북','')
+       else: it_coodi = text
+       price = row[8] # 년금액
+       price_sum += int(price) # 합계금액 
+
+       print("{0}/{1}/년{2}원/{3}/{4}/{5}".format(row[3], row[4].split()[0], commaParse(row[8]), it_coodi, row[1], row[5]))
+    """
+    # filter
+    filter_product = {'ICUBE 클라우드서버', 'ICUBE/IU 클라우드서버', 'ICUBE/GW 클라우드서버', 'IU 클라우드서버'
+        , 'IU/GW 클라우드서버', 'GW 클라우드서버', '클라우드서버'}
+
+    df5 = df4[df4['상품명'].apply(lambda x: x in filter_product)]
+
+    return df5
+
+def save_excel_file(data_file_path, save_df):
+    #df_dic = {'영업채널명': [], '영업담당자명':[], '사업자/주민':[], '고객명':[], '상품명':[], '판매유형':[], '공급가':[], '금액(VAT별도)':[]}
+    start, ext = os.path.splitext(data_file_path)  # ('C:\\Users\\김진태\\Jupyter_Folder\\Data\\order20190401to0419.xlsx', '.xlsx')
+    tail = '' + "_{:%Y%m%d%H%M%S}".format(datetime.datetime.now())  # '_20190503135732'
+    save_file_name = start + tail + ext
+    save_df.to_excel(save_file_name)
+
+
+def save_text_file(data_file_path, save_text):
+    start, ext = os.path.splitext(
+        data_file_path)  # ('C:\\Users\\김진태\\Jupyter_Folder\\Data\\order20190401to0419', '.xlsx')
+    tail = '' + "_{:%Y%m%d%H%M%S}".format(datetime.datetime.now())  # '_20190503135732'
+    save_file_name = start + tail + '.txt'
+
+    with open(save_file_name, 'w') as f:
+        f.write(save_text)
+
+
+def main():
+    print('{0}[{1:^21}]{2}'.format('=' * 20, '더존NSM 발주정보 확인(클라우드사업부 전용)', '=' * 20))
+    print('{:>72}'.format('2019.05.03 K.J.T'))
+    print('-'*76)
+
+    DATA_PATH = "C:\\Temp\\NsmData\\"
+    data_file_path = DATA_PATH + 'Sheet1.xlsx'
+
+    if not os.path.exists(data_file_path):
+        print('실패! ' + data_file_path + ' file not found!! ')
+        print('Exists files :')
+        print(os.listdir(DATA_PATH))
+        print('program exit... good-bye!')
+        exit()
+
+    # 프로그램 시작 시간 표시
+    start_dt = datetime.datetime.now()
+    ts = start_dt.timetuple()
+    print('지금 시간은  {}년 {}월 {}일 {}시 {}분 {}초'.format(ts.tm_year,ts.tm_mon, ts.tm_mday, ts.tm_hour, ts.tm_min, ts.tm_sec))
+
+    print('시작시간:' + start_dt.strftime('%Y-%m-%d, %H:%M:%S')) #'2019-04-19, 13:31:11'
+    print('=-='*25) # 줄긋기
+
+    print('reading file : data_file_path')
+    df = pd.read_excel(data_file_path, Sheet_name='Sheet1')
+
+    df2 = get_filter_dataframe(df)
+
+    #save_excel_file(data_file_path, df2)
+
+    # 업체명/솔루션/년금액/센터/담당자/판매유형
+    save_text = ''
+    price_sum = int()
+    for row in df2.values:
+        org_price = int(row[8]) # 년금액
+        price_sum += org_price  # 합계금액
+
+        channel_name = row[0].replace('IT코디센터', '').strip().replace('/경북', '')
+        product_name = row[5].split()[0].replace('/', ',')
+        order_type = '재계약' if row[6] == '기타' else row[6]
+        product_price = "{:,}".format(int(row[8]))
+        print_text = "- {0}/{1}/년{2}원/{3}/{4}/{5}".format(row[2], product_name, product_price, channel_name, row[1], order_type)
+        print(print_text)
+        save_text += print_text + '\n'
+
+
+    # 결과 갯수 표시
+    #count_row = df2.shape[0]  # gives number of row count   참고) len(df2)는 결과값이 같으나 느림
+    #count_col = df2.shape[1]  # gives number of col count
+    #print('{0}개가 검색되었습니다.'.format(df2.shape[0]))
+
+    comma_sum = "{:,}원".format(price_sum)
+    print_text = "총금액: {}({}건)".format(comma_sum, df2.shape[0])
+    print(print_text)
+    save_text += print_text + '\n'
+
+    print('-'*50)
+    # 업체명 (사업자번호)
+    for row in df2.values:
+        print_text = "- {0} ({1})".format(row[2], row[3])
+        print(print_text)
+        save_text += print_text + '\n'
+
+    save_text_file(data_file_path, save_text)
+
+    if df2.shape[0]:
+        save_file_path = DATA_PATH + 'order' + df2['주문일자'].values[0].replace('-', '') + "_{:%Y%m%d%H%M%S}".format(
+            datetime.datetime.now()) + '.xlsx'
+        print('changed file name : ' + save_file_path)
+        os.rename(data_file_path, save_file_path)
+
+    # 프로그램 종료 시간 표시
+    print('=-='*30) # 줄긋기
+    end_dt = datetime.datetime.now()
+    print("소요시간: {0}".format(end_dt - start_dt))
+    print('종료시간: ' + end_dt.strftime('%Y-%m-%d, %H:%M:%S')) #'2019-04-19, 13:31:11'
+
+    print('-' * 76)
+    print('create success! good-bye!')
+
+
+if __name__ == "__main__":
+    main()
---
+++ PythonFile/Work/매월_재계약_안내_쪽지_자동작성.py
@@ -0,0 +1,147 @@
+﻿import pandas
+import openpyxl
+#import re #regex
+import os
+from datetime import datetime
+
+def input_file_path():
+    #cur_dir = os.getcwd()
+    #print('current directory : ' + cur_dir)
+    print('program exit : \'x\' press key or \'Enter\' key')
+    while True:
+        input_data = input('input source file path : ')
+        if input_data == 'x' or input_data == '':
+            break
+        else:
+            if os.path.exists(input_data):
+                #print(input_data)
+                return input_data
+            else:
+                print('file not found : ' + input_data)
+    return ''
+
+def get_header_001(center_name, month):
+    header = """
+[재계약 리스트] {0}IT코디센터 2019년 {1} 재계약 리스트 전달 및 수주 등록 요청드립니다.
+수신 : {0}IT코디센터 센터장님, 영업담당자
+참조 : CLOUD사업팀
+발신 : 김진태 부장
+-----------------------------------------------
+안녕하십니까? 클라우드사업팀 김진태입니다
+
+클라우드서버 및 클라우드ERP 상품 관련하여 아래와 같이 재계약 리스트를 전달하오니
+관련 상품 재계약 여부 확인하시어 수주 등록 부탁드립니다.(재계약 NSM 발주)
+
+※ 고객사 재계약 여부 확인이 늦어저 해지 시 해당월 세금계산서 발행이 되지 않도록 빠른 회신 부탁드립니다.
+※ 재계약 등록 시 솔루션에 맞는 상품으로 진행 부탁드립니다.
+※ 계산서 발행월 기준 리스트로 누락 없이 진행 부탁드립니다.
+※ 당월 반품.해지등의 사유로 재계약 미진행 시 당월 이용요금이 청구될 수 있습니다.
+
+※ 해지 등의 사유로 미진행시 반드시 회신 부탁드립니다.
+   (쪽지 수신자 : 남원길 부장, 김진태 부장, 홍민순 과장, 김태은 대리 포함 쪽지 회신 요망)
+
+감사합니다.
+
+-------------------- 아   래 ------------------
+[ 2019년 {1} 재계약 리스트 ]
+
+센터 | 상품 | 납부 | 고객명 | 사업자번호 | 월금액 | 재계약금액 | 재계약월
+    """.format(center_name, month)
+    return header
+
+
+def text_print_001(ws_rows):
+    # pattern = re.compile('(?<=\d)(?=(\d{3})+(?!\d))')
+    pre_center_name = 'none'
+    result_text = ''
+    for row in ws_rows:
+        center_name = row[1].value
+
+        # 첫줄은 pass
+        if center_name == '센터':
+            pre_center_name = center_name
+            continue
+
+            # month_price = re.sub(pattern,',',str(row[6].value))
+        if 's' == row[6].data_type:  # 칼럼 헤더 구분
+            month_price = row[6].value
+            # print('{0}:{1}: row index = {2}'.format(row[6].value, type(row[6].value), row[6].row))
+        else:
+            month_price = "{:,}원".format(int(row[6].value))
+
+        # year_price = re.sub(pattern,',',str(row[7].value))
+        if 's' == row[7].data_type:  # 칼럼 헤더 구분
+            year_price = str(row[7].value)
+        else:
+            year_price = "{:,}원".format(int(row[7].value))
+
+        month = row[8].value  # 재계약월
+
+        # print('pre_center_name:{0} , center_name:{1}'.format(pre_center_name, center_name)) #debug
+
+        if pre_center_name != center_name:
+            if pre_center_name != '센터':
+                print_text = '\n{0}   끝   {1}\n'.format('-' * 20, '-' * 20)
+                result_text += print_text + '\n'
+                # print(print_text)
+            if pre_center_name != 'none':
+                print_text = get_header_001(center_name, month)
+                # print_text = '[ header ]' + '\n'  #debug
+                result_text += print_text + '\n'
+                # print(print_text)
+
+            pre_center_name = center_name
+
+        print_text = '{0} | {1} | {2} | {3} | {4} | {5} | {6} | {7}'.format(
+            row[1].value, row[2].value, row[3].value, row[4].value, row[5].value, month_price, year_price, row[8].value)
+        result_text += print_text + '\n'
+        # print(print_text)
+
+    # 마지막 라인에 '끝' 추가
+    print_text = '\n{0}   끝   {1}\n'.format('-' * 20, '-' * 20)
+    result_text += print_text + '\n'
+    # print(print_text)
+
+    return result_text
+
+def main():
+    print('{0}[{1:^21}]{2}'.format('=' * 20, '매월 재계약 안내 쪽지 자동작성', '=' * 20))
+    print('{:>72}'.format('2019.05.03 K.J.T'))
+    print('-'*76)
+
+    data_file_path = input_file_path()
+    if 0 >= len(data_file_path):
+        print('program exit... good-bye!')
+        exit()
+
+    # data_file_path = 'C:\\Users\\김진태\\Jupyter_Folder\\Data\\5월 재계약 업체 리스트_20190502.xlsx'
+
+    wb = openpyxl.load_workbook(data_file_path)
+
+    try:
+        ws = wb["Sheet1"]
+
+        # 첫줄을 읽어 칼럼명 리스트를 만든다
+        column_name_list = [cell.value for cell in ws[1]]
+
+        save_text = text_print_001(ws.rows)
+        # print(save_text)
+
+        #save_file_name = "C:\\Users\\김진태\\Jupyter_Folder\\Data\\재계약_쪽지_" + datetime.now().strftime('%Y%m%d') + ".txt"
+        start, ext = os.path.splitext(data_file_path)  # ('C:\\Users\\김진태\\Jupyter_Folder\\Data\\5월 재계약 업체 리스트_20190502', '.xlsx')
+        tail = '' + "_{:%Y%m%d%H%M%S}".format(datetime.now())  # '_20190503135732'
+        save_file_name = start + tail+ '.txt'
+
+        with open(save_file_name, 'w') as f:
+            f.write(save_text)
+
+    except Exception as e:
+        print('Exception : %s' % e)
+    finally:
+        wb.close()
+
+    print('-' * 76)
+    print('create success! good-bye!')
+
+if __name__ == "__main__":
+    main()
