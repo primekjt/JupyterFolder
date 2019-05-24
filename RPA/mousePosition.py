ebbbe8227988374e955d196b2d7ecafeb4e422ed
primekjt <primekjt@douzone.com>
2019-05-24 14:07:29 +0900

    로컬PC 업데이트 2019.05.24

---
+++ RPA/mousePosition.py
@@ -0,0 +1,23 @@
+import pyautogui
+
+"""
+command>python mousePosition.py
+"""
+
+def mousePosition():
+    print('Press Ctrl-C to quit.')
+    try:
+        while True:
+            # Get and print the mouse coordinates.
+            x, y = pyautogui.position()
+            positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+            print(positionStr, end='')
+            print('\b' * len(positionStr), end='', flush=True)
+    except KeyboardInterrupt:
+        print('\nDone.')
+
+def main():
+    mousePosition()
+
+if __name__ == '__main__':
+    main()
